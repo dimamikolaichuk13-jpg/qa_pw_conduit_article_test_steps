bee91d39613a2e314ae670c49eb7b5762294f7cb
Dmytro Mykolaichuk <dimamikolaichuk13@gmail.com>
2026-08-14 10:54:14 +0200

    Various tests have been created for the article on Conduit. And fixed test 'signInPositsve'.

--- src/pages/CreateArticlePage.js
+++ src/pages/CreateArticlePage.js
@@ -7,6 +7,12 @@ export class CreateArticlePage {
       name: 'Publish Article',
     });
     this.errorMessage = page.getByRole('list').nth(1);
+    this.titleField = page.getByPlaceholder('Article Title');
+    this.articleAboutField = page.getByPlaceholder(
+      "What's this article about?",
+    );
+    this.inputArticleField = page.getByPlaceholder('Write your article (in');
+    this.tagsField = page.getByPlaceholder('Enter tags');
   }
 
   async clickPublishArticleButton() {
@@ -18,6 +24,30 @@ export class CreateArticlePage {
   async assertErrorMessageContainsText(messageText) {
     await test.step(`Assert the '${messageText}' error is shown`, async () => {
       await expect(this.errorMessage).toContainText(messageText);
+    });
+  }
+
+  async fillTitleField(text) {
+    await test.step(`Fill in the article title field`, async () => {
+      await this.titleField.fill(text);
+    });
+  }
+
+  async fillArticleAboutField(text) {
+    await test.step(`Fill in the article about field`, async () => {
+      await this.articleAboutField.fill(text);
+    });
+  }
+
+  async fillInputArticleField(text) {
+    await test.step(`Fill in the "Write your article" field`, async () => {
+      await this.inputArticleField.fill(text);
+    });
+  }
+
+  async fillTagsField(text) {
+    await test.step(`Fill in the "WEnter tags" field`, async () => {
+      await this.tagsField.fill(text);
     });
   }
 }
--- tests/signIn/signInPositive.spec.js
+++ tests/signIn/signInPositive.spec.js
@@ -1,26 +1,41 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
+import { SignUpPage } from '../../src/pages/SignUpPage';
 import { SignInPage } from '../../src/pages/SignInPage';
 import { HomePage } from '../../src/pages/HomePage';
 
-let signInPage;
-let homePage;
-let user;
+test.describe('Sign in positive tests', () => {
+  let signUpPage;
+  let signInPage;
+  let homePage;
+  let user;
 
-test.beforeEach(async ({ page }) => {
-  signInPage = new SignInPage(page);
-  homePage = new HomePage(page);
+  test.beforeEach(async ({ page }) => {
+    signUpPage = new SignUpPage(page);
+    signInPage = new SignInPage(page);
+    homePage = new HomePage(page);
 
-  user = {
-    email: 'test_new_user@gmail.com',
-    password: 'newpass123!',
-  };
-});
+    const timestamp = Date.now();
+    user = {
+      username: `user${timestamp}`,
+      email: `user_${timestamp}@gmail.com`,
+      password: 'Password123!',
+    };
 
-test('Successful `Sign in` flow test', async () => {
-  await signInPage.open();
-  await signInPage.fillEmailField(user.email);
-  await signInPage.fillPasswordField(user.password);
-  await signInPage.clickSignInButton();
+    await signUpPage.open();
+    await signUpPage.fillUsernameField(user.username);
+    await signUpPage.fillEmailField(user.email);
+    await signUpPage.fillPasswordField(user.password);
+    await signUpPage.clickSignUpButton();
 
-  await homePage.assertYourFeedTabIsVisible();
+    await expect(homePage.yourFeedTab).toBeVisible();
+  });
+
+  test('Successful `Sign in` flow test', async () => {
+    await signInPage.open();
+    await signInPage.fillEmailField(user.email);
+    await signInPage.fillPasswordField(user.password);
+    await signInPage.clickSignInButton();
+
+    await expect(homePage.yourFeedTab).toBeVisible();
+  });
 });
